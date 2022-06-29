7ffe6d6d1ef2b24839a1ac07d9f6ec73b71f31d4
YuryHouse <yury.house@yahoo.com>
2022-06-29 21:26:01 +0300

    added mixins & variables scss

--- src/hiring/Hiring.js
+++ src/hiring/Hiring.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import style from './Hiring.module.css';
 import styleContainer from "../common/styles/Container.module.css";
+import Title from "../common/components/title/Title";
 
 function Hiring() {
     return (
         <div className={style.hiringBlock}>
             <div className={`${styleContainer.container} ${style.hiringContainer}`}>
-                <h2 className={style.title}>Looking to work remotely or relocate</h2>
+                <Title name={'Looking to work remotely or relocate'}/>
                 <div className={style.buttonContainer}>
                     <button>Hire мe</button>
                 </div>
