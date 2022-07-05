c77f5126d4e64e365d8723ef7fc24f321747c16a
YuryHouse <yury.house@yahoo.com>
2022-07-05 21:22:27 +0300

    edited nav, header, hire and contact blocks

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import style from './Contact.module.css'
+import style from './Contact.module.scss'
 import styleContainer from "../common/styles/Container.module.css";
+import Title from "../common/components/title/Title";
 
 function Contact() {
     return (
         <div className={style.contactBlock}>
             <div className={`${styleContainer.container} ${style.contactContainer}`}>
-                <h2 className={style.title}>Contact me</h2>
+                <Title name={'Contact me'}/>
                 <form className={style.contact}>
                     <input type={"text"} />
                     <input type={"text"} />
--- src/hiring/Hiring.js
+++ src/hiring/Hiring.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './Hiring.module.css';
+import style from './Hiring.module.scss';
 import styleContainer from "../common/styles/Container.module.css";
 import Title from "../common/components/title/Title";
 
