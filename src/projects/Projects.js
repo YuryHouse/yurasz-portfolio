632d74e416973db1bc431e4c49270251eba9cc65
YuryHouse <yury.house@yahoo.com>
2022-08-21 20:41:57 +0300

    Projects.js has been fixed

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -4,6 +4,7 @@ import styleContainer from '../common/styles/Container.module.css'
 import Project from "./project/Project";
 import Title from "../common/components/title/Title";
 import todoImage from "./../assets/image/todolist.jpg";
+import cardsImage from "./../assets/image/cardsImage.jpg"
 import socialImage from "./../assets/image/social.jpg";
 import agencyImage from "./../assets/image/agency.jpg";
 import Fade from "react-reveal/Fade";
@@ -12,6 +13,9 @@ import Fade from "react-reveal/Fade";
 function Projects() {
     const social = {
         backgroundImage: `url(${todoImage})`
+    };
+    const cards = {
+        backgroundImage: `url(${cardsImage})`
     };
     const list = {
         backgroundImage: `url(${socialImage})`
@@ -23,11 +27,16 @@ function Projects() {
         <div className={style.projectsBlock}>
             <div className={`${styleContainer.container} ${style.projectsContainer}`}>
                 <Title name={"My Projects"}/>
-                <Fade bottom>
-                    <div className={style.projects}>
+                <div className={style.projects}>
+                    <Fade bottom>
                         <Project
                             style={list}
                             title={'Social network'}
+                            description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
+                                'sed do eiusmod tempor incididuntut labore et dolore magna aliqua.'}/>
+                        <Project
+                            style={cards}
+                            title={'Learning Cards'}
                             description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' +
                                 'sed do eiusmod tempor incididuntut labore et dolore magna aliqua.'}/>
                         <Project
@@ -41,8 +50,8 @@ function Projects() {
                             style={agency}
                             title={'Estate agency'}
                             description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}/>
-                    </div>
-                </Fade>
+                    </Fade>
+                </div>
             </div>
         </div>
     );
