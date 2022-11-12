ccea037f59eab1cb0bd4a13c24f250a80be3c75f
YuryHouse <yury.house@yahoo.com>
2022-11-12 23:06:44 +0300

    Contact form has been updated

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -5,59 +5,77 @@ import styleContainer from "../common/styles/Container.module.css";
 import Title from "../common/components/title/Title";
 import Fade from "react-reveal/Fade";
 import {Box, TextField} from "@mui/material";
+import axios from "axios";
+import {useForm} from "react-hook-form";
 
 function Contact() {
+    const {register, formState: {errors}, handleSubmit} = useForm();
+    const onSubmit = ({name, email, message}, e) => {
+        e.preventDefault()
+        axios.post('http://localhost:3010/sendMessage', {name, email, message})
+            .then(() => {
+                alert('Thank you! Your message has been send.');
+            });
+        e.target.reset()
+    }
     return (
         <div className={style.contactBlock} id={'contact'}>
             <div className={`${styleContainer.container} ${style.contactContainer}`}>
                 <Title name={'Contact me'}/>
                 <Fade right cascade>
-                    <Box component="form"
-                         sx={{
-                             '& .MuiTextField-root': {m: 1, width: '30ch'}, input: { color: 'white'},
-                         }}
-                         noValidate
-                         autoComplete="off"
-                    >
-                        <div className={style.lineInput}>
-                            <div>
+                    <form onSubmit={handleSubmit(onSubmit)} id={'contact-form'}>
+                        <Box component="form"
+                             sx={{
+                                 '& .MuiTextField-root': {m: 1, width: '30ch'}, input: {color: 'white'},
+                             }}
+                             noValidate
+                             autoComplete="off"
+                        >
+                            <div className={style.lineInput}>
                                 <div>
-                                    <TextField id="outlined-basic"
-                                               label="Name" variant="outlined"
-                                               color="primary"
-                                               focused
-                                               required={true}
-                                               fullWidth
-                                    />
+                                    <div>
+                                        <TextField label="Name" variant="outlined"
+                                                   color="primary"
+                                                   focused
+                                                   fullWidth
+                                                   placeholder={'Your name'}
+                                                   type={'text'}
+                                                   {...register('name', {required: true})}
+                                        />
+                                        {errors.name && <p style={{color: '#fff'}}>Name is required</p>}
+                                    </div>
+                                    <div>
+                                        <TextField id="outlined-basic"
+                                                   label="e-mail"
+                                                   variant="outlined"
+                                                   color="primary"
+                                                   focused
+                                                   fullWidth
+                                                   {...register('email', {
+                                                       required: true,
+                                                       pattern: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+                                                   })} placeholder={'E-mail'} type={'text'}
+                                        />
+                                        {errors.email && <p style={{color: '#fff'}}>E-mail is required</p>}
+                                    </div>
                                 </div>
                                 <div>
-                                    <TextField id="outlined-basic"
-                                               label="e-mail"
-                                               variant="outlined"
-                                               color="primary"
-                                               focused
-                                               required={true}
-                                               fullWidth
+                                    <TextField
+                                        id="outlined-multiline-static"
+                                        label="Message"
+                                        multiline
+                                        rows={4}
+                                        color="primary"
+                                        focused
+                                        fullWidth
+                                        inputProps={{style: {color: "white"}}}
+                                        {...register('message', {required: true})}
                                     />
-
                                 </div>
                             </div>
-                            <div>
-                                <TextField
-                                    id="outlined-multiline-static"
-                                    label="Message"
-                                    multiline
-                                    rows={4}
-                                    color="primary"
-                                    focused
-                                    fullWidth
-                                    required={true}
-                                    inputProps={{ style: { color: "white" } }}
-                                />
-                            </div>
-                        </div>
-                    </Box>
-                    <button className={`${styleButton.button}`} type="submit">Send</button>
+                        </Box>
+                        <button className={`${styleButton.button} ${style.button}`} type="submit">Send</button>
+                    </form>
                 </Fade>
             </div>
         </div>
